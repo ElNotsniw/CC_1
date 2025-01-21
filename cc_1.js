aa2f5ebecead33097413e564bffc7ca4d96ae20b
ElNotsniw <winstonjosh010604@gmail.com>
2025-01-21 18:27:19 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -5,3 +5,11 @@ var isActive = true;
 console.log("employeename:", employeename, "Type:", typeof employeename);
 console.log("employeeID:", employeeID, "Type:", typeof employeeID);
 console.log("isActive:", isActive, "Type:", typeof isActive);
+
+let productName = "Laptop";
+const productPrice = 500;
+var isAvailable = true;
+
+console.log("productName:", productName, "Type:", typeof productName);
+console.log("productPrice:", productPrice, "Type:", typeof productPrice);
+console.log("isAvailable:", isAvailable, "Type:", typeof isAvailable);
