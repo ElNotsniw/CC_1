3025ee5b55ba6b07ed7193dc175dbcbfc07fd912
ElNotsniw <winstonjosh010604@gmail.com>
2025-01-21 18:26:09 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+let employeename = "Winston Le";
+const employeeID = 5266623;
+var isActive = true;
+
+console.log("employeename:", employeename, "Type:", typeof employeename);
+console.log("employeeID:", employeeID, "Type:", typeof employeeID);
+console.log("isActive:", isActive, "Type:", typeof isActive);
