9854ffa3e842d6c9f2d22ca6de2fe8c0bcca3d5f
ElNotsniw <winstonjosh010604@gmail.com>
2025-01-23 11:52:28 -0500

    Task 5 - Access Control

--- cc_1.js
+++ cc_1.js
@@ -1,3 +1,4 @@
+// Task 1: Employee Information
 let employeename = "Winston Le";
 const employeeID = 5266623;
 var isActive = true;
@@ -6,6 +7,7 @@ console.log("employeename:", employeename, "Type:", typeof employeename);
 console.log("employeeID:", employeeID, "Type:", typeof employeeID);
 console.log("isActive:", isActive, "Type:", typeof isActive);
 
+//Task 2: Products Details
 let productName = "Laptop";
 const productPrice = 500;
 var isAvailable = true;
@@ -14,14 +16,21 @@ console.log("productName:", productName, "Type:", typeof productName);
 console.log("productPrice:", productPrice, "Type:", typeof productPrice);
 console.log("isAvailable:", isAvailable, "Type:", typeof isAvailable);
 
+//Task 3: Financial Transactions
 let accountBalance = 100;
 let deposit = 200;
 let TotalBalance = accountBalance + deposit;
 
 console.log("TotalBalance:", TotalBalance);
 
+//Task 4: Customer Messaging
 let customerName = "Leonard,";
 let Welcome = " Welcome to to our website!"
 let message = customerName.concat(Welcome);
 
 console.log(message);
+
+//Task 5: Access Control
+let isLoggedIn = true
+
+console.log("Logged in:", isLoggedIn)
